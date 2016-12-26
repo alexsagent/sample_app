85a531671c4315e9a054e8149d93674368b24003
alexsagent <alexsagent@gmail.com>
2016-12-26 18:14:08 +0300

    sample app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-
+  resources :users
   root  'static_pages#home'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
