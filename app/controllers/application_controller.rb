2a5f3e0e0ca5827e6f53aa170acacd3b0854a44a
alexsagent <alexsagent@gmail.com>
2017-02-06 22:51:14 +0300

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  rescue_from Exception, with: :not_found
+  #rescue_from Exception, with: :not_found
 
 private
 
