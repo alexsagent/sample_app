7cdd15b4c38935d42b98c63b4638f9e36f904758
alexsagent <alexsagent@gmail.com>
2017-02-20 13:40:54 +0300

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+
+MAPS_LINK_NAME = 'Maps'
+APP_NAME = 'oDistGen'
+HOME_LINK_NAME = 'Home'
+USERS_LINK_NAME = 'Users'
+PROFILE_MENU_NAME = 'Profile'
+SETTINGS_MENU_NAME = 'Settings'
+SIGN_OUT_LINK = 'Sign out'
+SIGN_IN = 'Sign in'
+SIGN_UP_NOW = 'Sign up now!'
+SIGN_UP = 'Sign up'
+ABOUT_LINK = 'About'
+ABOUT_US = 'About Us'
+ABOUT_US_TEXT = 'This is a application which generates random distances for orienteering maps'
+CONTACT_LINK = 'Contact'
+VIEW_PROFILE_LINK = 'view my profile'
+EDIT_USER = 'Edit User'
+CONFIRM_PASS = 'Confirm Password'
+SAVE_CHANGES = 'Save changes'
+ALL_USERS = 'All Users'
+CONFIRM = 'Confirmation'
+CRT_MY_ACC = 'Create my account'
+HELP = 'Help'
+
+
+
+
 end
