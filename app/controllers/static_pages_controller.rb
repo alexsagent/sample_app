18636fa85d4ffe672b6665da9b37010f189f025e
alexsagent <alexsagent@gmail.com>
2017-02-28 22:20:15 +0300

    test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page], per_page: 3)
     end
-    @message_items = Message.paginate(page: params[:page], per_page: 5)
+    @message_items = Message.paginate(page: params[:page], per_page: 4)
 
   end
 
